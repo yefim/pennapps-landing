ca73c68fce2b95a237949beeb37d151b30bbaa74
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-07-24 12:31:03 -0400

    helpful comments

--- mail.rb
+++ mail.rb
@@ -11,8 +11,8 @@ Pony.mail to: "yefim323@gmail.com",
             address: 'smtp.gmail.com',
             port: '587',
             enable_starttls_auto: true,
-            user_name: EMAIL,
-            password: PASSWORD,
+            user_name: EMAIL, # set in config.rb to 'yefim323' or 'naruto137'
+            password: PASSWORD, # your gmail pass
             authentication: :plain,
             domain: 'localhost:4567'
           }
