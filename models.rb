b345e2a1d5a8fab6d0e717a9865e67d3ea8befab
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-07-23 20:45:42 -0400

    Added models

---
+++ models.rb
@@ -0,0 +1,15 @@
+require 'data_mapper'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :email, String
+  property :recommendation_url, String
+  has n, :users, :through => Resource
+end
+
+DataMapper.auto_migrate!
